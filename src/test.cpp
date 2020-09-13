f5b633753dd3bf433459689526b5adc28c057b73
MillerMC96 <pywang@ucdavis.edu>
2020-09-13 15:54:21 -0700

    setting up file to read

--- src/test.cpp
+++ src/test.cpp
@@ -2,5 +2,7 @@
 #include "gromacs/fileio/xtcio.h"
 
 int main(void) {
-    std::cout << "Testing" << std::endl;
+    const char* filename = "text_xtc/chainH_protein_H_50-60ns_pbc_whole.xtc";
+    std::cout << "Testing read:" << std::endl;
+    std::cout << "reading file: " << filename << std::endl;
 }
