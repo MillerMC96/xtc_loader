5e0b7040fd41594f00e35c9933bd121490afec05
MillerMC96 <pywang@ucdavis.edu>
2020-09-23 11:07:19 -0700

    fixed spell error

--- src/test.cpp
+++ src/test.cpp
@@ -2,7 +2,7 @@
 #include "gromacs/fileio/xtcio.h"
 
 int main(void) {
-    const char* filename = "text_xtc/chainH_protein_H_50-60ns_pbc_whole.xtc";
+    const char* filename = "../test_xtc/chainH_protein_H_50-60ns_pbc_whole.xtc";
     struct t_fileio* xtc_file;
     std::cout << "Testing read:" << std::endl;
     std::cout << "reading file: " << filename << std::endl;
